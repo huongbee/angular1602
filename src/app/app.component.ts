6dcc964dfc02fa21f44fe9d2475cd5ee8265ec4c
Huong <huongnguyenak96@gmail.com>
2019-04-20 14:42:43 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-root',
+  // selector: '.app-root',
+  selector: '[app-root]',
+  // templateUrl: './app.component.html',
+  template: `
+      <h2>Hello Angular</h2>
+  `,
+  styles: [
+    `h2{
+      color:red
+    }
+    `,
+    'h2{color:blue}'
+  ]
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular1602';
