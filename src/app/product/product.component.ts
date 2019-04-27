df3308eb436394e071afbd4a655807b2009dd612
Huong <huongnguyenak96@gmail.com>
2019-04-27 15:36:55 +0700

    addToWishlist

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,13 +9,17 @@ export class ProductComponent implements OnInit {
 
   // listProduct: Product[] = []
   listProduct: Array<Product> = [
-    {id: 1, name: 'Iphone X', price: 20000000, wishlish: true},
-    {id: 2, name: 'Macbook Pro', price: 40000000, wishlish: true},
-    {id: 3, name: 'Apple Watch', price: 30000000, wishlish: true}
+    {id: 1, name: 'Iphone X', price: 20000000, wishlist: true},
+    {id: 2, name: 'Macbook Pro', price: 40000000, wishlist: false},
+    {id: 3, name: 'Apple Watch', price: 30000000, wishlist: true}
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+  addToWishlist(id) {
+    const product = this.listProduct.find(p => p.id === id );
+    product.wishlist = true;
   }
 
 }
@@ -23,5 +27,5 @@ interface Product {
   id: number;
   name: string;
   price: number;
-  wishlish: boolean;
+  wishlist: boolean;
 }
