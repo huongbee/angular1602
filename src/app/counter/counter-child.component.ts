b69dbb98f255356dad7f930a4ccb0de5921714ec
Huong <huongnguyenak96@gmail.com>
2019-05-04 16:13:36 +0700

    dispath

--- src/app/counter/counter-child.component.ts
+++ src/app/counter/counter-child.component.ts
@@ -5,14 +5,22 @@ import { OnInit, Component } from '@angular/core';
 @Component({
   selector: 'app-counter-child',
   template: `
-  <button>Increment</button>
-  <button>Decrement</button>
-  <button>Reset</button>
+  <button (click)="increase()">Increment</button>
+  <button (click)="decrease()">Decrement</button>
+  <button (click)="reset()">Reset</button>
   `,
   styleUrls: ['./counter.component.css']
 })
 export class CounterChildComponent {
-    constructor() {
-
+    constructor(private store: Store<any>) {
+    }
+    increase() {
+        this.store.dispatch({type: 'INCREASE'});
+    }
+    decrease() {
+        this.store.dispatch({type: 'DECREASE'});
+    }
+    reset() {
+        this.store.dispatch({type: 'RESET'});
     }
 }
