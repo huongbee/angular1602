187ecbb78b72fd1e8b93f0050834bda4418a0dee
Huong <huongnguyenak96@gmail.com>
2019-05-11 15:01:51 +0700

    form

--- src/app/request-api/request-api.component.ts
+++ src/app/request-api/request-api.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../product.class';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-request-api',
@@ -10,12 +11,26 @@ import { Product } from '../product.class';
             <ul>
                 <li *ngFor="let p of products"> {{p.name}} - {{p.price | number}}</li>
             </ul>
+            <h3>Add product</h3>
+            <form [formGroup]="productForm"
+            (ngSubmit)="addProduct()">
+                Name: <input formControlName="name" placaholder="Enter name">
+                <br><br>
+                Price: <input formControlName="price" placaholder="Enter price">
+                <br><br>
+                <button class="btn btn-success">Add</button>
+            </form>
         </div>
     `
 })
 export class RequestApiComponent implements OnInit {
     products: Product[];
-    constructor(private http: HttpClient) {
+    productForm: FormGroup;
+    constructor(private http: HttpClient, private fb: FormBuilder) {
+        this.productForm = this.fb.group({
+            name: ['', Validators.required],
+            price: ['0', Validators.required]
+        });
     }
     ngOnInit() {
         // this.http.get('https://api.openweathermap.org/data/2.5/find?appid=01cc37655736835b0b75f2b395737694&q=Saigon')
